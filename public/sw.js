539e3ba9ce9a54cbd5827381a53fc05ebb2726c2
bulkbun-res <bulkbun@outlook.com>
2026-08-09 17:18:09 +0200

    chore: bump sw version to bypass cache on clients

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bulkbun-pwa-v1";
+const CACHE_NAME = "bulkbun-pwa-v2";
 const ASSETS = ["/", "/favicon.png", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", (e) => {
